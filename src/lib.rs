40b36018504b2ade22da2cff78843d1657ec2277
Paul Easter <paul.john.easter@gmail.com>
2023-03-20 15:24:10 +1100

    Able to make a draft post now

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod schema;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenvy::dotenv;
+use self::models::{NewPost,Post};
 use std::env;
 
 pub fn establish_connection() -> PgConnection {
@@ -13,4 +14,15 @@ pub fn establish_connection() -> PgConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_post(conn: &mut PgConnection, title: &str, body: &str) -> Post {
+    use crate::schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .get_result(conn)
+        .expect("Error saving new post")
 }
--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,5 @@
 use diesel::prelude::*;
+use crate::schema::posts;
 
 #[derive(Queryable)]
 pub struct Post {
@@ -6,4 +7,11 @@ pub struct Post {
     pub title: String,
     pub body: String,
     pub published: bool,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = posts)]
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub body: &'a str,
 }
